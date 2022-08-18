4df48d37b883d917fe7713d17d84476f15243025
Julio Alberto de la Teja López <jdelatejal242@profesor.uemex.mx>
2022-08-17 22:15:26 -0500

    ultimo commit se agrega el aun más texto

--- src/Principal.java
+++ src/Principal.java
@@ -2,5 +2,6 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("primer commit");
         System.out.println("segundo commit");
+        System.out.println("tercer commit");
     }
 }
